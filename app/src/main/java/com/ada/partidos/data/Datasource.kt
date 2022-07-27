853a5fdea800b67a12788b9ef65512e3d470966f
sami1793 <sami_17_94@hotmail.com>
2022-07-26 21:23:50 -0300

    Se agregaron los otros 3 partidos del grupo C. Estos cambios se realizaron sobre string.xml para poner los titulos de partidos y Datasource para traer estos nombres y su imagen

--- app/src/main/java/com/ada/partidos/data/Datasource.kt
+++ app/src/main/java/com/ada/partidos/data/Datasource.kt
@@ -7,8 +7,11 @@ class Datasource {
     fun cargarPartidos():List<Partido>{
         return listOf<Partido>(
             Partido(R.string.Fecha1,R.drawable.argentina,R.drawable.arabia),
-            Partido(R.string.Fecha2,R.drawable.argentina,R.drawable.mexico),
-            Partido(R.string.Fecha3,R.drawable.polonia,R.drawable.argentina)
+            Partido(R.string.Fecha2,R.drawable.mexico,R.drawable.polonia),
+            Partido(R.string.Fecha3,R.drawable.polonia,R.drawable.arabia),
+            Partido(R.string.Fecha4,R.drawable.argentina,R.drawable.mexico),
+            Partido(R.string.Fecha5,R.drawable.polonia, R.drawable.argentina),
+            Partido(R.string.Fecha6,R.drawable.arabia, R.drawable.mexico)
         )
     }
 }
